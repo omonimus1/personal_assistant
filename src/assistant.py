8cdaf7e68e15e50be0013e565c4605f8df313a2b
omonimus1 <davidepollicino2015@gmail.com>
2020-04-30 11:50:34 +0100

    Added language code filter for wikipedia search

--- src/assistant.py
+++ src/assistant.py
@@ -7,6 +7,7 @@ import psutil  # https://github.com/giampaolo/psutil
 import speech_recognition as sr
 import pyttsx3
 import os
+import wikipediaapi
 engine = pyttsx3.init()
 import json
 import helloUser
@@ -14,6 +15,71 @@ import helloUser
 
 r = sr.Recognizer()
 file_path = 'data.json'
+
+"""
+    Wikipedia query search
+"""
+# Ask to the user in which language he would like to fetch the wiki
+def get_result_language():
+    print('Inside get result language')
+    engine.say('I can provide you information in English,' + 
+        'Swedish, German, French, Russian, Italian or Spanish')
+    engine.say('Which language do you prefer for your wikipedia search?')
+    language = ask_to_user()
+    if language == 'English':
+        language_code = 'en'
+        return language_code
+    elif language == 'Swedish':
+        language_code = 'sv'
+        return language_code
+    elif language == 'German':
+        language_code = 'de'
+        return language_code
+    elif language == 'French':
+        language_code = 'fr'
+        return language_code
+    elif language == 'Russian':
+        language_code = 'ru'
+        return language_code
+    elif language == 'Italian':
+        language_code = 'it'
+        return language_code
+    elif language == 'Spanish':
+        language_code = 'es'
+        return language_code
+    else:
+        engine.say('Sorry, I did not get the language '+
+            'o the language that you have indicated is not available, try again' )
+        get_result_language()
+
+
+def get_page_to_search():
+    engine.say('What would you like to search?')
+    page = ask_to_user()
+    return page
+
+def wikipedia_search():
+    language_code = get_result_language()
+    # Filter the search on a specific language
+    wiki_wiki = wikipediaapi.Wikipedia(language_code)
+    page_name = get_page_to_search()
+    # Check if the page exists
+    page_py = wiki_wiki.page(page) 
+    # If the page does not exists, repeat search process
+    if( not page_py.exists()):
+        print('Soomethhing went wrong during the search, page does not exists')
+        engine.say('The page that your are trying to search does not exists')
+        engine.say('Please, be more specific')
+        wikipedia_search()
+    else:
+        # Provide information found
+        print('Printing page information')
+        print(page_py.fullurl)
+        engine.say(page_py.title)
+        print(page_py.summary[0:60])
+        engine.say(page_py.summary[0:150])
+        
+        
 
 """
     User profile Management 
@@ -32,14 +98,14 @@ def load_default_user_data():
     return json_str
 
 
-def ask_name_to_user():
+def ask_to_user():
     with sr.Microphone() as source:
-        print("Say your name")
+        print("Say something")
         audio = r.listen(source)
         print("Time over, THANKS")
     try:
-        username = r.recognize_google(audio)
-        return username
+        vocal_command = r.recognize_google(audio)
+        return vocal_command
     except:
         engine.say('I am sorry, there was an error while getting your name')
         pass
@@ -87,9 +153,10 @@ def list_functionalities():
 
 # Main
 def main():
+    wikipedia_search()
     # assistant_introduction()
     # helloUser.current_time()
-    # username = ask_name_to_user()
+    # username = ask_to_user()
     # print(username)
     # helloUser.hello_to_user(username)
     # helloUser.say_today_date()
